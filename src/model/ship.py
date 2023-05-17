9fce5a7fb5c9b6a16603f7b6395f24d03187c96d
Felipe Rodrigues de Santana Freitag <felipefreitag08@hotmail.com>
2023-05-17 16:06:55 -0300

    feat: implement constructor checks for valid positions in the ship

--- src/model/ship.py
+++ src/model/ship.py
@@ -2,17 +2,45 @@ from src.model.position import Position
 from src.model.ship_type import ShipType
 
 
+class ShipException(Exception):
+    def __init__(self, message: str):
+        super().__init__(message)
+
+
 class Ship:
     def __init__(self, type: ShipType, initial_pos: Position, end_pos: Position):
+        Ship.__check_ship(type, initial_pos, end_pos)
         self.__type = type
         self.__initial_pos = initial_pos
         self.__end_pos = end_pos
 
-    def get_initial_pos(self) -> Position:
+    @property
+    def initial_pos(self) -> Position:
         return self.__initial_pos
 
-    def get_end_pos(self) -> Position:
+    @property
+    def end_pos(self) -> Position:
         return self.__end_pos
 
-    def get_type(self) -> ShipType:
+    @property
+    def type(self) -> ShipType:
         return self.__type
+
+    def __check_ship(type: ShipType, initial_pos: Position, end_pos: Position):
+        if type == ShipType.SUBMARINE and initial_pos != end_pos:
+            raise ShipException("O submarino deve começar e terminar na mesma posição")
+
+        initial_x, initial_y = initial_pos.get()
+        end_x, end_y = end_pos.get()
+
+        if initial_x != end_x and initial_y != end_y:
+            raise ShipException("Os submarinos não pode estar na diagonal")
+
+        diff = abs(initial_y - end_y) if initial_x == end_x else abs(initial_x - end_x)
+
+        if type == ShipType.SMALL_SHIP and diff != 1:
+            raise ShipException("O submarino pequeno deve ter dois de tamanho")
+        elif type == ShipType.MEDIUM_SHIP and diff != 2:
+            raise ShipException("O submarino médio deve ter três de tamanho")
+        elif type == ShipType.BIG_SHIP and diff != 3:
+            raise ShipException("O submarino grande deve ter quatro de tamanho")
