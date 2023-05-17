182e719d3fa53b275abab84f7113f5acd77918ed
Felipe Rodrigues de Santana Freitag <felipefreitag08@hotmail.com>
2023-05-17 15:57:11 -0300

    test: add tests for position equality

--- tests/model/test_position.py
+++ tests/model/test_position.py
@@ -9,3 +9,24 @@ def test_constructor():
     expected = (x, y)
 
     assert position.get() == expected
+
+
+def test_equal_positions_equality():
+    x = 10
+    y = 5
+
+    position_one = Position(x, y)
+    position_two = Position(x, y)
+
+    assert position_one == position_two
+
+def test_different_positions_equality():
+    x_one = 10
+    y_one = 5
+    x_two = 20
+    y_two = 15
+
+    position_one = Position(x_one, y_one)
+    position_two = Position(x_two, y_two)
+
+    assert position_one != position_two
