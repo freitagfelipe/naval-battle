464ba310712a5be837f2016174879b026b03c8fb
Felipe Rodrigues de Santana Freitag <felipefreitag08@hotmail.com>
2023-05-17 21:51:17 -0300

    test: add the tests of the ship board model

---
+++ tests/model/test_ships_board.py
@@ -0,0 +1,87 @@
+import pytest
+from src.model.ships_board import ShipBoard, InvalidShip, Ship
+from src.model.ship_type import ShipType
+from src.model.position import Position
+
+def test_set_ship_should_fail_with_initial_x_outside_grid():
+    with pytest.raises(InvalidShip, match="O x da posição inicial está fora do tabuleiro"):
+        ship_board = ShipBoard(10, 10)
+
+        ship = Ship(ShipType.SMALL_SHIP, Position(-1, 0), Position(0, 0))
+
+        ship_board.set_ship(ship)
+
+    with pytest.raises(InvalidShip, match="O x da posição inicial está fora do tabuleiro"):
+        ship_board = ShipBoard(10, 10)
+
+        ship = Ship(ShipType.SMALL_SHIP, Position(10, 0), Position(9, 0))
+
+        ship_board.set_ship(ship)
+
+
+def test_set_ship_should_fail_with_end_x_outside_grid():
+    with pytest.raises(InvalidShip, match="O x da posição final está fora do tabuleiro"):
+        ship_board = ShipBoard(10, 10)
+
+        ship = Ship(ShipType.SMALL_SHIP, Position(0, 0), Position(-1, 0))
+
+        ship_board.set_ship(ship)
+
+    with pytest.raises(InvalidShip, match="O x da posição final está fora do tabuleiro"):
+        ship_board = ShipBoard(10, 10)
+
+        ship = Ship(ShipType.SMALL_SHIP, Position(9, 0), Position(10, 0))
+
+        ship_board.set_ship(ship)
+
+
+def test_set_ship_should_fail_with_initial_y_outside_grid():
+    with pytest.raises(InvalidShip, match="O y da posição inicial está fora do tabuleiro"):
+        ship_board = ShipBoard(10, 10)
+
+        ship = Ship(ShipType.SMALL_SHIP, Position(0, -1), Position(0, 0))
+
+        ship_board.set_ship(ship)
+
+    with pytest.raises(InvalidShip, match="O y da posição inicial está fora do tabuleiro"):
+        ship_board = ShipBoard(10, 10)
+
+        ship = Ship(ShipType.SMALL_SHIP, Position(0, 10), Position(0, 9))
+
+        ship_board.set_ship(ship)
+
+
+def test_set_ship_should_fail_with_end_y_outside_grid():
+    with pytest.raises(InvalidShip, match="O y da posição final está fora do tabuleiro"):
+        ship_board = ShipBoard(10, 10)
+
+        ship = Ship(ShipType.SMALL_SHIP, Position(0, 0), Position(0, -1))
+
+        ship_board.set_ship(ship)
+
+    with pytest.raises(InvalidShip, match="O y da posição final está fora do tabuleiro"):
+        ship_board = ShipBoard(10, 10)
+
+        ship = Ship(ShipType.SMALL_SHIP, Position(0, 9), Position(0, 10))
+
+        ship_board.set_ship(ship)
+
+
+def test_set_a_ship_should_pass():
+    ship_board = ShipBoard(10, 10)
+
+    ship = Ship(ShipType.BIG_SHIP, Position(5, 5), Position(8, 5))
+
+    ship_board.set_ship(ship)
+
+def test_set_a_ship_where_already_has_a_ship_should_fail():
+    with pytest.raises(InvalidShip, match="A posição já está ocupada"):
+        ship_board = ShipBoard(10, 10)
+
+        ship = Ship(ShipType.BIG_SHIP, Position(5, 5), Position(8, 5))
+
+        ship_board.set_ship(ship)
+
+        second_ship = Ship(ShipType.BIG_SHIP, Position(6, 3), Position(6, 6))
+
+        ship_board.set_ship(second_ship)
