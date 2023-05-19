73f9017259c1ba34e709960eac39d589ef3071a3
Felipe Rodrigues de Santana Freitag <felipefreitag08@hotmail.com>
2023-05-19 00:41:41 -0300

    feat: add functions in the game controller to construct models

--- src/controller/game_controller.py
+++ src/controller/game_controller.py
@@ -1,15 +1,33 @@
 from src.model.player.player import Player
 from src.model.position.position import Position
 from src.util.enums.current_player import CurrentPlayer
+from src.model.boards.ship_board import ShipBoard, Ship
 from src.model.boards.guesses_board import GuessesBoard, GuessType
+from src.DTOs.ship_dto import ShipDTO
 from typing import Tuple
 
 
 class GameController:
-    def __init__(self, player_one: Player, player_two: Player):
+    def __init__(self):
         self.__current_player = CurrentPlayer.PLAYER_ONE
-        self.__player_one = player_one
-        self.__player_two = player_two
+        self.__player_one_ship_board = ShipBoard(10, 10)
+        self.__player_two_ship_board = ShipBoard(10, 10)
+
+    def set_player_ship(self, player: CurrentPlayer, ship_dto: ShipDTO):
+        ship = Ship(ship_dto.type, ship_dto.initial_pos, ship_dto.end_pos)
+        
+        if player == CurrentPlayer.PLAYER_ONE:
+            self.__player_one_ship_board.set_ship(ship)
+
+            return
+        
+        self.__player_two_ship_board.set_ship(ship)
+
+    def construct_players(self, player_one_name: str, player_two_name: str):
+        print(f"Aqui: {self.__player_two_ship_board.ships}")
+
+        self.__player_one = Player(player_one_name, GuessesBoard(self.__player_two_ship_board))
+        self.__player_two = Player(player_two_name, GuessesBoard(self.__player_one_ship_board))
 
     def compute_player_guess(self, position: Position) -> GuessType:
         if self.__current_player == CurrentPlayer.PLAYER_ONE:
