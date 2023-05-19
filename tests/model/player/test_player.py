29843fbcdde6aee419068137568fff854ceee08f
Felipe Rodrigues de Santana Freitag <felipefreitag08@hotmail.com>
2023-05-18 21:24:46 -0300

    test: update player constructor in tests

--- tests/model/player/test_player.py
+++ tests/model/player/test_player.py
@@ -8,7 +8,7 @@ from src.model.ship.ship import Ship
 
 
 def test_player_make_guess_in_water_should_pass():
-    player = Player("jogador", ShipBoard(10, 10), ShipBoard(10, 10))
+    player = Player("jogador", ShipBoard(10, 10))
     old_score = player.score
     player.make_guess(Position(4, 5))
 
@@ -24,7 +24,7 @@ def test_player_make_guess_same_position_hited_before_should_fail():
             Ship(ShipType.SUBMARINE, Position(5, 7), Position(5, 7))
         )
 
-        player = Player("jogador", ShipBoard(10, 10), enemy_ships_board)
+        player = Player("jogador", enemy_ships_board)
 
         player.make_guess(Position(5, 7))
         player.make_guess(Position(5, 7))
@@ -34,7 +34,7 @@ def test_player_make_guess_in_ship_should_pass():
     enemy_ships_board = ShipBoard(10, 10)
     enemy_ships_board.set_ship(Ship(ShipType.SUBMARINE, Position(5, 7), Position(5, 7)))
 
-    player = Player("jogador", ShipBoard(10, 10), enemy_ships_board)
+    player = Player("jogador", enemy_ships_board)
     old_score = player.score
 
     player.make_guess(Position(5, 7))
@@ -44,19 +44,19 @@ def test_player_make_guess_in_ship_should_pass():
 
 def test_player_make_invalid_guess_should_fail():
     with pytest.raises(GuessesBoardException):
-        player = Player("jogador", ShipBoard(10, 10), ShipBoard(10, 10))
+        player = Player("jogador", ShipBoard(10, 10))
 
         player.make_guess(Position(11, 10))
 
 
 def test_name_getter():
-    player = Player("jogador", ShipBoard(10, 10), ShipBoard(10, 10))
+    player = Player("jogador", ShipBoard(10, 10))
 
     assert "jogador" == player.name
 
 
 def test_round_score_getter():
-    player = Player("jogador", ShipBoard(10, 10), ShipBoard(10, 10))
+    player = Player("jogador", ShipBoard(10, 10))
 
     player.set_round_score()
 
@@ -64,6 +64,6 @@ def test_round_score_getter():
 
 
 def test_ships_bord_getter():
-    player = Player("jogador", ShipBoard(2, 2), ShipBoard(2, 2))
+    player = Player("jogador", ShipBoard(2, 2))
 
-    assert ShipBoard(2, 2) == player.ships_board
+    assert ShipBoard(2, 2) == player.guesses_board
