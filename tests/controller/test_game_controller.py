e28d4a80180c5160de16cbddc2392cee55e11440
Felipe Rodrigues de Santana Freitag <felipefreitag08@hotmail.com>
2023-05-19 00:41:56 -0300

    test: update game controller tests

--- tests/controller/test_game_controller.py
+++ tests/controller/test_game_controller.py
@@ -1,30 +1,31 @@
-from src.controller.game_controller import GameController, Player, Position, GuessType
+from src.controller.game_controller import GameController, Player, Position, GuessType, CurrentPlayer
+from src.DTOs.ship_dto import ShipDTO
 from src.model.boards.ship_board import ShipBoard
-from src.model.ship.ship import Ship, ShipType
+from src.model.ship.ship import ShipType
 
 
-def test_compute_alternate_player():
-    ship_board = ShipBoard(10, 10)
+def test_alternate_player():
+    game_controller = GameController()
 
-    player_one = Player("Felipe", ship_board)
-    player_two = Player("Henok", ship_board)
-
-    game_controller = GameController(player_one, player_two)
+    game_controller.construct_players("Felipe", "Henok")
 
     assert game_controller.alternate_player() == "Henok"
     assert game_controller.alternate_player() == "Felipe"
 
 
 def test_compute_player_guess():
-    ship_board = ShipBoard(10, 10)
+    ship_one = ShipDTO(ShipType.SUBMARINE, Position(5, 5), Position(5, 5))
+    ship_two = ShipDTO(ShipType.SMALL_SHIP, Position(6, 5), Position(7, 5))
 
-    ship_board.set_ship(Ship(ShipType.SUBMARINE, Position(5, 5), Position(5, 5)))
-    ship_board.set_ship(Ship(ShipType.SMALL_SHIP, Position(6, 5), Position(7, 5)))
+    game_controller = GameController()
 
-    player_one = Player("Felipe", ship_board)
-    player_two = Player("Henok", ship_board)
+    game_controller.set_player_ship(CurrentPlayer.PLAYER_ONE, ship_one)
+    game_controller.set_player_ship(CurrentPlayer.PLAYER_ONE, ship_two)
 
-    game_controller = GameController(player_one, player_two)
+    game_controller.set_player_ship(CurrentPlayer.PLAYER_TWO, ship_one)
+    game_controller.set_player_ship(CurrentPlayer.PLAYER_TWO, ship_two)
+
+    game_controller.construct_players("Felipe", "Henok")
 
     assert game_controller.compute_player_guess(Position(5, 5)) == GuessType.DESTROYED
 
@@ -38,15 +39,18 @@ def test_compute_player_guess():
 
 
 def test_get_player_score():
-    ship_board = ShipBoard(10, 10)
+    ship_one = ShipDTO(ShipType.SUBMARINE, Position(5, 5), Position(5, 5))
+    ship_two = ShipDTO(ShipType.SMALL_SHIP, Position(6, 5), Position(7, 5))
 
-    ship_board.set_ship(Ship(ShipType.SUBMARINE, Position(5, 5), Position(5, 5)))
-    ship_board.set_ship(Ship(ShipType.SMALL_SHIP, Position(6, 5), Position(7, 5)))
+    game_controller = GameController()
 
-    player_one = Player("Felipe", ship_board)
-    player_two = Player("Henok", ship_board)
+    game_controller.set_player_ship(CurrentPlayer.PLAYER_ONE, ship_one)
+    game_controller.set_player_ship(CurrentPlayer.PLAYER_ONE, ship_two)
 
-    game_controller = GameController(player_one, player_two)
+    game_controller.set_player_ship(CurrentPlayer.PLAYER_TWO, ship_one)
+    game_controller.set_player_ship(CurrentPlayer.PLAYER_TWO, ship_two)
+
+    game_controller.construct_players("Felipe", "Henok")
 
     game_controller.compute_player_guess(Position(5, 5)) == GuessType.DESTROYED
 
@@ -54,20 +58,16 @@ def test_get_player_score():
 
     game_controller.alternate_player()
 
-    assert game_controller.compute_player_guess(Position(6, 5)) == GuessType.HIT
+    game_controller.compute_player_guess(Position(6, 5)) == GuessType.HIT
 
     assert game_controller.current_player_score == 1
 
-
 def test_get_players_board():
-    ship_board = ShipBoard(10, 10)
+    game_controller = GameController()
 
-    player_one = Player("Felipe", ship_board)
-    player_two = Player("Henok", ship_board)
-
-    game_controller = GameController(player_one, player_two)
+    game_controller.construct_players("Felipe", "Henok")
 
     player_one_board, player_two_board = game_controller.players_board
 
-    assert player_one_board == ship_board
-    assert player_two_board == ship_board
+    assert player_one_board == ShipBoard(10, 10)
+    assert player_two_board == ShipBoard(10, 10)
