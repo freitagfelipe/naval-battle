637acf532eadd21a87889a479343762906aff049
Felipe Rodrigues de Santana Freitag <felipefreitag08@hotmail.com>
2023-05-19 00:52:41 -0300

    feat: implement the ShipDTO

---
+++ src/DTOs/ship_dto.py
@@ -0,0 +1,20 @@
+from src.util.enums.ship_type import ShipType
+from src.model.position.position import Position
+
+class ShipDTO:
+    def __init__(self, type: ShipType, initial_pos: Position, end_pos: Position):
+        self.__type = type
+        self.__initial_pos = initial_pos
+        self.__end_pos = end_pos
+
+    @property
+    def type(self):
+        return self.__type
+    
+    @property
+    def initial_pos(self):
+        return self.__initial_pos
+    
+    @property
+    def end_pos(self):
+        return self.__end_pos
