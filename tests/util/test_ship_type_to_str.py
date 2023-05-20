969c9f0789f13829678dd5f56d90aac8f45345b7
Henok Teixeira <henokteixeira@gmail.com>
2023-05-19 23:51:39 -0300

    test: add ship type to str case tests

---
+++ tests/util/test_ship_type_to_str.py
@@ -0,0 +1,13 @@
+from naval_battle.util.ship_type_to_str import ship_type_to_str, ShipType
+
+def test_submarine_to_str():
+    assert "submarino" == ship_type_to_str(ShipType.SUBMARINE) 
+    
+def test_small_ship_to_str():
+    assert "navio pequeno" == ship_type_to_str(ShipType.SMALL_SHIP) 
+    
+def test_medium_ship_to_str():
+    assert "navio médio" == ship_type_to_str(ShipType.MEDIUM_SHIP) 
+    
+def test_big_ship_to_str():
+    assert "navio grande" == ship_type_to_str(ShipType.BIG_SHIP) 
