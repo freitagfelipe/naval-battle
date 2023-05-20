5dc0822c2ec1b1c8ee4a576b5f7714b18c50fda8
Felipe Rodrigues de Santana Freitag <felipefreitag08@hotmail.com>
2023-05-20 01:00:23 -0300

    test: remove the player round score test

--- tests/model/player/test_player.py
+++ tests/model/player/test_player.py
@@ -58,15 +58,6 @@ def test_name_getter():
 
     assert "jogador" == player.name
 
-
-def test_round_score_getter():
-    player = Player("jogador", GuessesBoard(ShipBoard(10, 10)))
-
-    player.set_round_score()
-
-    assert player.round_score == 1
-
-
 def test_ships_bord_getter():
     player = Player("jogador", GuessesBoard(ShipBoard(2, 2)))
 
