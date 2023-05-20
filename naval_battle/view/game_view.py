2e859e11f11162a3f6db4c576572549e0c2c1407
Henok Teixeira <henokteixeira@gmail.com>
2023-05-19 23:40:22 -0300

    feat: add read player ship funtion with private aux functions in game view

--- naval_battle/view/game_view.py
+++ naval_battle/view/game_view.py
@@ -1,7 +1,14 @@
+from naval_battle.DTOs.ship_dto import ShipDTO
 from naval_battle.controller.game_controller import GameController, CurrentPlayer
+from naval_battle.model.boards.ship_board import InvalidShip
+from naval_battle.model.position.position import Position
+from naval_battle.model.ship.ship import ShipException
+from naval_battle.util.ship_type_to_str import ship_type_to_str
+from naval_battle.util.enums.ship_type import ShipType
 from termcolor import cprint
 import os
 import re
+
 
 
 class GameView:
@@ -33,7 +40,7 @@ class GameView:
 
             self.__print_warning_message(
                 "O nome só pode conter caracteres de a-z ou A-Z"
-            )  # RED
+            )  
 
         self.__clear()
 
@@ -48,7 +55,7 @@ class GameView:
 
             self.__print_warning_message(
                 "O nome só pode conter caracteres de a-z ou A-Z"
-            )  # RED
+            )  
 
     def before_reading_player_ships(self, player: CurrentPlayer):
         self.__clear()
@@ -71,7 +78,70 @@ class GameView:
         input("Pressione enter para continuar...")
 
     def read_player_ships(self, player: CurrentPlayer):
+        ships_to_read = [ShipType.SUBMARINE, ShipType.SMALL_SHIP, ShipType.SMALL_SHIP, ShipType.MEDIUM_SHIP, ShipType.MEDIUM_SHIP, ShipType.BIG_SHIP]
+        
+        for ship in ships_to_read:
+            self.__clear()
+            print(self.__game_controller.get_player_ship_board(player), end="\n\n")
+            self.__read_ship(player, ship)
+            
         self.__clear()
+        print(self.__game_controller.get_player_ship_board(player), end="\n\n")
+        input("Pressione enter para continuar...")
+        
+        
+        
+    def __read_ship(self, player: CurrentPlayer, ship_type: ShipType):
+        submatine_message = "Digite a posição, no formato 'X,Y', do"
+        message_initial_pos = "Digite a posição inicial, no formato 'X,Y', do"
+        message_end_pos = "Digite a posição final, no formato 'X,Y', do"
+         
+                
+        while True:
+            if ship_type == ShipType.SUBMARINE:
+                ship_initial_position = self.__read_position(f"{submatine_message} {ship_type_to_str(ship_type)}, com tamanho {ship_type.value}: ", player)
+                ship_end_position = ship_initial_position
+            else: 
+                ship_initial_position = self.__read_position(f"{message_initial_pos} {ship_type_to_str(ship_type)}, com tamanho {ship_type.value}: ", player)
+                ship_end_position =self.__read_position(f"{message_end_pos} {ship_type_to_str(ship_type)}, com tamanho {ship_type.value}: ", player)
+                
+            ship_dto = ShipDTO(ship_type, ship_initial_position, ship_end_position)
+            
+            try:
+                self.__game_controller.set_player_ship(player, ship_dto)
+                return
+            except InvalidShip as error:
+                self.__clear()
+                print(self.__game_controller.get_player_ship_board(player), end="\n\n")
+                print(f"Posição inicial digitada: {ship_initial_position}")
+                print(f"Posição final digitada: {ship_end_position}")
+                self.__print_warning_message(error)
+            except ShipException as error:
+                self.__clear()
+                print(self.__game_controller.get_player_ship_board(player), end="\n\n")
+                print(f"Posição inicial digitada: {ship_initial_position}")
+                print(f"Posição final digitada: {ship_end_position}")
+                self.__print_warning_message(error)
+                
+
+    
+    def __read_position(self, message_input: str, player: CurrentPlayer) -> Position:
+        pattern = r"^\d+,\d+$"
+        ship_position = ""
+        
+        while True:
+            ship_position = input(f"{message_input}")
+            
+            if re.match(pattern, ship_position):
+                break
+            
+            self.__clear()
+            print(self.__game_controller.get_player_ship_board(player), end="\n\n")
+            self.__print_warning_message("A posição só pode conter dígitos entre vírgulas (ex: 1,4)")
+            
+        x,y = ship_position.split(",")
+        
+        return Position(int(x), int(y))
 
     def __clear(self):
         if os.name == "posix":
