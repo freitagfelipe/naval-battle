1ad8cb7580308fef1346a8ec9b3d88b987147f9f
Henok Teixeira <henokteixeira@gmail.com>
2023-05-19 23:52:41 -0300

    refactor: change submarine first letter to lower case

--- naval_battle/util/ship_type_to_str.py
+++ naval_battle/util/ship_type_to_str.py
@@ -2,7 +2,7 @@ from naval_battle.util.enums.ship_type import ShipType
 
 def ship_type_to_str(ship_type: ShipType) -> str:
     if ship_type == ShipType.SUBMARINE: 
-        return "Submarino"
+        return "submarino"
     elif ship_type == ShipType.SMALL_SHIP:
         return "navio pequeno"
     elif ship_type == ShipType.MEDIUM_SHIP:
