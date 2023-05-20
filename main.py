74b4015d4dae43167e9cefff427721b2f499ef01
Felipe Rodrigues de Santana Freitag <felipefreitag08@hotmail.com>
2023-05-20 15:09:55 -0300

    feat: update the main loop with all the game stages

--- main.py
+++ main.py
@@ -23,6 +23,31 @@ while True:
 
             game_view.read_player_ships(CurrentPlayer.PLAYER_TWO)
 
-            game_phase = None
+            game_view.construct_players()
+
+            game_phase = GamePhase.BEFORE_PLAY
+        case GamePhase.BEFORE_PLAY:
+            game_view.before_play()
+            game_view.start()
+
+            game_phase = GamePhase.PLAYING
+        case GamePhase.PLAYING:
+            game_view.make_guess()
+
+            if game_view.some_winner():
+                game_phase = GamePhase.AFTER_PLAY
+
+            game_view.alternate_player()
+        case GamePhase.AFTER_PLAY:
+            game_view.show_winner()
+
+            game_phase = GamePhase.PLAY_AGAIN
+        case GamePhase.PLAY_AGAIN:
+            if game_view.play_again():
+                game_view.reset()
+            
+                game_phase = game_phase.READ_NAMES
+            else:
+                game_phase = None
         case _:
             break
