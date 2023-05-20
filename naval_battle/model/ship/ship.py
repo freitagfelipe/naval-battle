df4456dd4ad81b47695334f1834f69890138a067
Henok Teixeira <henokteixeira@gmail.com>
2023-05-19 23:38:02 -0300

    fix: fix exceptions messages in model ship

--- naval_battle/model/ship/ship.py
+++ naval_battle/model/ship/ship.py
@@ -39,13 +39,13 @@ class Ship:
         end_x, end_y = end_pos.get()
 
         if initial_x != end_x and initial_y != end_y:
-            raise ShipException("Os submarinos não pode estar na diagonal")
+            raise ShipException("O navio não pode estar na diagonal")
 
         diff = abs(initial_y - end_y) if initial_x == end_x else abs(initial_x - end_x)
 
         if type == ShipType.SMALL_SHIP and diff != 1:
-            raise ShipException("O submarino pequeno deve ter dois de tamanho")
+            raise ShipException("O navio pequeno deve ter dois de tamanho")
         elif type == ShipType.MEDIUM_SHIP and diff != 2:
-            raise ShipException("O submarino médio deve ter três de tamanho")
+            raise ShipException("O navio médio deve ter três de tamanho")
         elif type == ShipType.BIG_SHIP and diff != 3:
-            raise ShipException("O submarino grande deve ter quatro de tamanho")
+            raise ShipException("O navio grande deve ter quatro de tamanho")
